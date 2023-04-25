5cd359f451b59f4917cc4683b47dc2763151f476
Karmanya <2203311@mancurad.mancurad.iitgoa.ac.in>
2023-04-25 11:45:33 +0530

    added comments

--- code/2203311_data_filter.sh
+++ code/2203311_data_filter.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-file=$1
-name=$2
-value=$3
+file=$1		#storing file name in a variable
+name=$2		#storing filed_name in a variable
+value=$3	#storing field_value in a variable
 
-output=${file::2}$name$value
-touch $output.csv 
-echo -n "$file $name $value " > $output.csv 
-grep "$value" $file | wc -l  >> $output.csv
+output=${file::2}$name$value	#namig the output file
+touch $output.csv 	#creating the output file
+echo -n "$file $name $value " > $output.csv 	#writing first line of the file
+grep "$value" $file | wc -l  >> $output.csv	#getting the number of lines meeting given criteria
 
-head -n 1 $file >> $output.csv
+head -n 1 $file >> $output.csv		#getting the heading lines using the head command
 
-grep "$value" $file >> $output.csv
+grep "$value" $file >> $output.csv	#getting the data with give field_name and field_value
